fd6db0ba6e615f594689706772a66e816103c412
jacob87o2 <jacob87@o2.pl>
2020-02-04 23:25:58 +0100

    feat(sample/13-mongo-typeorm): add terminus module

--- sample/13-mongo-typeorm/src/app.module.ts
+++ sample/13-mongo-typeorm/src/app.module.ts
@@ -3,6 +3,23 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
 import { PhotoModule } from './photo/photo.module';
 import { Photo } from './photo/photo.entity';
+import { TerminusModule, TerminusModuleOptions, TypeOrmHealthIndicator } from "@nestjs/terminus";
+
+const getTerminusOptions = (
+  db: TypeOrmHealthIndicator,
+): TerminusModuleOptions => ({
+  endpoints: [
+    {
+      // The health check will be available with /health
+      url: '/health',
+      // All the indicator which will be checked when requesting /health
+      healthIndicators: [
+        // Set the timeout for a response to 300ms
+        async () => db.pingCheck('database', { timeout: 300 })
+      ],
+    },
+  ],
+});
 
 @Module({
   imports: [
@@ -12,6 +29,11 @@ import { Photo } from './photo/photo.entity';
       database: 'test',
       entities: [Photo],
       synchronize: true,
+    }),
+    TerminusModule.forRootAsync({
+      // Inject the TypeOrmHealthIndicator provided by nestjs/terminus
+      inject: [TypeOrmHealthIndicator],
+      useFactory: db => getTerminusOptions(db),
     }),
     PhotoModule,
   ],
